4648d103d1107b493ffed9ba082a9f883ad2be8b
Abhinav <abhinavsinha135@gmail.com>
2024-11-12 17:53:56 +0530

    code pushed

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="cashier-copyright-area">
+        <div className="cashier-copyright text-center bg-themeBlue bg-footer h-20 leading-[80px] mt-20">
+          <span className="text-[15px] text-white font-normal">
+           FutureTech Tycoons
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
